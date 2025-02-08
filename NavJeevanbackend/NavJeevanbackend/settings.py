17b2b7f664a8449b4622b98cc521e81096aecb9c
chauh_harsh <chauhanharsh226@gmail.com>
2025-02-09 04:52:53 +0530

    Added Requirement.txt

--- NavJeevanbackend/NavJeevanbackend/settings.py
+++ NavJeevanbackend/NavJeevanbackend/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-s@c60lsc#0ox6sv%u3q15&(lg50z-6yo_s)0e3493855$sclym
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['.render.com', 'navjeevan-35jk.onrender.com', '127.0.0.1', 'localhost']
+
 
 
 # Application definition
